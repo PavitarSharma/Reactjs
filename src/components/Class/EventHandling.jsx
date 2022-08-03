40bc0c32d50979100500a4dad57a9c0c0973091e
PavitarSharma <pavitarsharma144@gmail.com>
2022-08-04 00:57:41 +0530

    event handling reactjs

--- src/components/Class/EventHandling.jsx
+++ src/components/Class/EventHandling.jsx
@@ -51,17 +51,23 @@ class EventHandling extends Component {
         return (
             <div>
                {/*  Bind Method First */}
+               {/* Binding Event Handler in the Constructor */}
                 <button onClick={this.handdleClick} style={{ background: "#000000", color: "#fff", padding: "10px 14px", border: "0", margin: "20px"}}>
                     {this.state.isToggleOn ? "ON" : "OFF"}
                 </button>
 
                 {/*  Bind Method Second */}
+                {/* Binding Event Handler using Arrow Function: 
+                This is pretty much the same approach as above, however, 
+                in this approach we are binding the event handler implicitly. 
+                This approach is the best if you want to pass parameters to your event. */}
                 <button onClick={() => this.handdleClick()} style={{ background: "#000000", color: "#fff", padding: "10px 14px", border: "0", margin: "20px"}}>
                     Login
                 </button>
 
 
                 {/*  Bind Method Third */}
+                {/* Binding Event Handler in Render Method: We can bind the handler when it is called in the render method using bind() method.  */}
                 <h1>{this.state.name}</h1>
                 <input type="text" placeholder='Name' value={this.state.name} onChange={this.handleChange.bind(this)}/>
 
